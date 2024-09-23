05816ff3e666e65824921c889951bdd13a3143b2
Evian-dev <evian.rebic@gmail.com>
2024-09-23 21:08:55 +0200

    Add files via upload

---
+++ uvodnaVjezba3/uvodnaVjezba3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+
+    }
+    
+    {
+        // Deklaracija varijabla
+        int a = 10;
+        int b = 7;
+        double c = 12.5;
+        double d = 16.7;
+
+        // Izračunavanje prosječne vrijednosti
+        double average = (a + b + c + d) / 4;
+
+        // Ispis prosječne vrijednosti
+        Console.WriteLine("Prosječna vrijednost: " + average);
+    }
+}
+
+
